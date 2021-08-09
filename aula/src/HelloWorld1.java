843feba765fef780e099e1f923d581b7e4e0d059
João Pedro <61983943+joaopdrost@users.noreply.github.com>
2021-08-08 21:09:59 -0300

    Update HelloWorld1.java

--- aula/src/HelloWorld1.java
+++ aula/src/HelloWorld1.java
@@ -1,10 +1,13 @@
-public class HelloWorld1{
-public static void main(String[] args) {
+   public class HelloWorld1
+{
+   public static void main(String[] args) 
+{
 	String mensagem = new HelloWorld1().retornaMensagem();
 	System.out.println(mensagem);
 }
-
-public String retornaMensagem(){
+	
+    public String retornaMensagem()
+{
 	return ("Hello World");
 }
 }
